651e7d0f5d9f750eb2537394a47192f9abadd064
Terry Teng <terry.teng@oocl.com>
2018-07-26 16:59:47 +0800

    [Terry] company add

---
+++ src/main/java/com/oocl/company/controllers/CompanyController.java
@@ -0,0 +1,31 @@
+package com.oocl.company.controllers;
+
+
+import com.oocl.company.entities.Company;
+import com.oocl.company.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+
+@RestController
+@RequestMapping("/api/v1/companies")
+public class CompanyController {
+
+    private CompanyRepository repositories;
+
+    @Autowired
+    public CompanyController(CompanyRepository repositories){
+        this.repositories = repositories;
+    }
+
+    //@Transactional
+    @PostMapping(path = "")//, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Company save(@RequestBody Company company){
+        return repositories.save(company);
+    }
+
+//    @Transactional
+//    @GetMapping(path = "", )
+}
---
+++ src/main/java/com/oocl/company/entities/Company.java
@@ -0,0 +1,55 @@
+package com.oocl.company.entities;
+
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.ZonedDateTime;
+
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @CreatedDate
+    private ZonedDateTime create_date = ZonedDateTime.now();
+
+    public Company(){}
+    public Company(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ZonedDateTime getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(ZonedDateTime create_date) {
+        this.create_date = create_date;
+    }
+
+}
